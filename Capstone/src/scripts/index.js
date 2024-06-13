a2b55179e9dda8758c6c2d44836c5939545a44c7
Rafly Fikri Fahrezi Ahmad <146017713+rflyffa@users.noreply.github.com>
2024-06-13 22:39:47 +0700

    Update index.js

--- Capstone/src/scripts/index.js
+++ Capstone/src/scripts/index.js
@@ -1,4 +1,25 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/no-useless-path-segments */
+/* eslint-disable import/extensions */
+/* eslint-disable eol-last */
 import 'regenerator-runtime';
 import '../styles/main.css';
+import renderNavbar from '../scripts/navbar.js';
+import renderCarousel from './view/home/carousel.js';
+import renderFooter from '../scripts/footer.js';
 
-console.log('Hello Coders! :)');
+// Membuat elemen <div> untuk menampung navbar, carousel, dan footer
+const navbarContainer = document.createElement('div');
+const carouselContainer = document.createElement('div');
+const footerContainer = document.createElement('div');
+
+// Memasukkan kode HTML navbar, carousel, dan footer ke dalam elemen <div>
+navbarContainer.innerHTML = renderNavbar();
+carouselContainer.innerHTML = renderCarousel();
+footerContainer.innerHTML = renderFooter();
+
+// Menambahkan elemen <div> ke dalam body HTML
+document.body.prepend(navbarContainer);
+document.body.insertBefore(carouselContainer, navbarContainer);
+document.body.append(footerContainer);

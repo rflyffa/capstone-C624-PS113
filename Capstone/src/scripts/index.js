019596f4248144cb24c04a6841ad1b120bbf9288
Rafly Fikri Fahrezi Ahmad <146017713+rflyffa@users.noreply.github.com>
2024-06-19 15:08:25 +0700

    Update header

--- Capstone/src/scripts/index.js
+++ Capstone/src/scripts/index.js
@@ -1,25 +1,9 @@
-/* eslint-disable import/no-extraneous-dependencies */
-/* eslint-disable no-unused-vars */
-/* eslint-disable import/no-useless-path-segments */
-/* eslint-disable import/extensions */
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable arrow-parens */
+/* eslint-disable indent */
+/* eslint-disable semi */
 /* eslint-disable eol-last */
+/* eslint-disable import/no-unresolved */
 import 'regenerator-runtime';
 import '../styles/main.css';
-import renderNavbar from '../scripts/navbar.js';
-import renderCarousel from './view/home/carousel.js';
-import renderFooter from '../scripts/footer.js';
-
-// Membuat elemen <div> untuk menampung navbar, carousel, dan footer
-const navbarContainer = document.createElement('div');
-const carouselContainer = document.createElement('div');
-const footerContainer = document.createElement('div');
-
-// Memasukkan kode HTML navbar, carousel, dan footer ke dalam elemen <div>
-navbarContainer.innerHTML = renderNavbar();
-carouselContainer.innerHTML = renderCarousel();
-footerContainer.innerHTML = renderFooter();
-
-// Menambahkan elemen <div> ke dalam body HTML
-document.body.prepend(navbarContainer);
-document.body.insertBefore(carouselContainer, navbarContainer);
-document.body.append(footerContainer);
+import '../styles/booking.css';
